6076832287bce9e1626c99d0e8a9e617e5383562
Silvia-C <33365850+Silvia-C@users.noreply.github.com>
2017-11-27 10:39:01 -0500

    change21

--- set_a_new_password.py
+++ set_a_new_password.py
@@ -1,22 +1,43 @@
-old_password = input("Please enter your old password: ")
-Password = input("Please set your password now: ")
-
-tries = 0
 
 
 
+def new_password():
+  
 
-while tries < 3:
+  tries = 0
+  
+  while tries < 3:
+    
+    Password = input("Please set your password now: ")
   
     tries += 1
     while Password != old_password:
       new_password = input("Please confirm your new password: ")
-      while new_password == Password:
+      if new_password == Password:
         print ("Your password has been saved!")
-      print("Sorry the passwords do not match, try again!")
-      new_password = input("Please confirm your new password: ")
+        return new_password
+      else:
+        print("Sorry the passwords do not match, try again!")
+        tries +=1
+    if Password == old_password:
+      print("You can't use your old password!")
+      
       tries +=1
-    print("You can't use your old password!")
-    Password = input("Please set your password now: ")
-    tries += 1
-print("You have reached the max attempts.")
+  else:
+    print("You have reached the max attempts.")
+  
+
+
+yn = input("Do you need to reset your password? ")
+if yn == "Y" or yn == "y":
+  old_password = input("Please enter your old password: ")
+  new_password()
+  
+elif yn == "N" or yn == "n":
+  print("Bye!")
+else:
+  print("You can only input Y/N")
+  
+
+
+      
