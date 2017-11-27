6f850722bcfa23dea93e5dd9a77a23e3acf4ca3a
Silvia-C <33365850+Silvia-C@users.noreply.github.com>
2017-11-27 02:45:53 -0500

    Add files via upload

---
+++ set_a_new_password.py
@@ -0,0 +1,28 @@
+Password = input("Please set your password now: ")
+tries = 0
+
+
+
+
+while tries < 3:
+  
+    entry = input("Please confirm your new password: ")
+  
+    tries += 1
+  if entry == Password:
+    
+    print ("Your password has been saved!")
+
+    print("You can navigate away now")
+
+    
+    break
+
+  elif  entry != Password and tries < 3:
+
+    print("Sorry the passwords do not match, try again!")
+    
+    
+  
+  else:
+    print ("You're locked out, call Mateusz")
